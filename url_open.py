b19c27bba406142ff96ca4cb7bb3e00e57999efb
pabloalima81 <pabloandrades@gmail.com>
2020-04-25 21:12:09 -0300

    Atualizacao para python3.5

--- url_open.py
+++ url_open.py
@@ -1,6 +1,5 @@
-from urllib.request import urlopen 
-
-# Retrieve HTML string from the URL
-html = urlopen("http://www.ifsul.edu.br")
-
-print(html.read())
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen("https://www.google.com")
+res = BeautifulSoup(html.read(),"html5lib");
+print(res.title)
