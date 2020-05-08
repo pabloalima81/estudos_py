5a0ccf970aca734ad3fcef74084f875f8f4fc044
pabloalima81 <pabloandrades@gmail.com>
2020-05-07 21:00:22 -0300

    Arquivo de ER atualizado

--- scraping/er.py
+++ scraping/er.py
@@ -3,7 +3,10 @@ import re
 
 padrao = "texto" #texto a ser procurado
 texto = "Este e um exemplo de texto para busca da palavra texto"
-resultado = re.search(padrao,texto)
+resultado = re.findall(padrao,texto)
 
-print(resultado) #localizacao do obj
-print(resultado.group()) #imprime o obj
+if resultado:
+	print(resultado) #localizacao do obj
+	#print(resultado.group()) #imprime o objeto
+else:
+	print("Nao Encontrado")
